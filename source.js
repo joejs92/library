62b8f74e7cd670c7ab6dc40b7e586413d8c5dd45
joejs92 <joejs_92@yahoo.com>
2024-01-09 10:46:31 -0500

    add a class for the class course

--- source.js
+++ source.js
@@ -1,26 +1,34 @@
 const myLibrary = [];
 
-function Book(author, title, number, read, comments) {
-    this.author = author;
-    this.title = title;
-    this.number = number;
-    this.read = read;
-    this.comments = comments;
-    let book = [author, title, number, read, comments];
-    myLibrary.push(book);
-    const card = document.createElement('div');
-    card.style.background = 'white';
-    card.style.height = '100px';
-    card.style.width = '200px';
-    card.style.margin = '8px';
-    card.style.overflow = 'auto';
-    for(let i = 0; i < book.length; i++){
-        const cardContent = document.createElement('p');
-        card.appendChild(cardContent);
-        cardContent.textContent = `${book[i]}`;
+class Book {
+    constructor(author, title, number, read, comments) {
+        this.author = author;
+        this.title = title;
+        this.number = number;
+        this.read = read;
+        this.comments = comments;
     }
-    let left = document.querySelector('.left');
-    left.appendChild(card);
+    addToMyLibrary() {
+        let book = [this.author, this.title, this.number, this.read, this.comments];
+        let len = book.length;
+        myLibrary.push(book);
+        return {book, len};
+    }
+    addToPage() {
+        const card = document.createElement('div');
+        card.style.background = 'white';
+        card.style.height = '100px';
+        card.style.width = '200px';
+        card.style.margin = '8px';
+        card.style.overflow = 'auto';
+        for(let i = 0; i < this.addToMyLibrary().len; i++){
+            const cardContent = document.createElement('p');
+            card.appendChild(cardContent);
+            cardContent.textContent = `${this.addToMyLibrary().book[i]}`;
+        }
+        let left = document.querySelector('.left');
+        left.appendChild(card);
+    }
 }
 
 function addBookToLibrary() {
@@ -30,7 +38,9 @@ function addBookToLibrary() {
     let number = document.getElementById('page_number').value;
     let read = document.getElementById('read').value;
     let comments = document.getElementById('comments').value;
-    Book(author, title, number, read, comments);
+    let newBook = new Book(author, title, number, read, comments);
+    newBook.addToMyLibrary();
+    newBook.addToPage();
 }
 
 const buttons = document.querySelectorAll('button');
